31ac9aac0570108f5eeba13aad061dc480264b61
KranePch <teevisit_kn@hotmail.com>
2023-02-28 23:07:07 +0700

    Add chat-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatPageComponent } from './chat-page/chat-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: 'chat/:id', component: ChatPageComponent, }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
